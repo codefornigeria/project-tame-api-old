ac41bc2cc242cc975b03ec26930596552f55152f
Inioluwa <adeb6600@gmail.com>
2017-04-12 10:03:33 +0100

    transform id for entity hooks

--- src/services/entity/hooks/index.js
+++ src/services/entity/hooks/index.js
@@ -2,7 +2,7 @@
 
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
-
+const mongoose = require('mongoose')
 
 const findByDomain = options => {
   console.log('showing optio', options)
@@ -14,10 +14,21 @@ const findByDomain = options => {
     Promise.resolve(hook)
   }
 }
+const  transformIds  = options =>{
+  return hook =>{
+    if(Array.isArray(hook.params.query._ids)){
+      hook.params.query._ids.map(function (id){
+        return mongoose.Types.ObjectId(id)
+      })
+    }
+  console.log('shoiwng changed hook ', hook )
+    Promise.resolve(hook)
+  }
+}
 
 exports.before = {
   all: [],
-  find: [findByDomain()],
+  find: [transformIds(),findByDomain()],
   get: [],
   create: [],
   update: [],
